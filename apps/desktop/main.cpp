705033d1899f41e674085804b091ccc88b3727cf
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 20:40:23 +0700

    feat: register digital timing renderer

--- apps/desktop/main.cpp
+++ apps/desktop/main.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+#include "digital_item.hpp"
 #include "document_controller.hpp"
 #include "waveform_item.hpp"
 
@@ -11,9 +12,10 @@ int main(int argc, char* argv[]) {
     QGuiApplication app(argc, argv);
     QGuiApplication::setApplicationName(QStringLiteral("ardirec"));
     QGuiApplication::setOrganizationName(QStringLiteral("ardirec"));
-    QGuiApplication::setApplicationVersion(QStringLiteral("0.2.0-alpha.1"));
+    QGuiApplication::setApplicationVersion(QStringLiteral("0.2.0-alpha.3"));
 
     qmlRegisterType<WaveformItem>("Ardirec.Render", 1, 0, "WaveformItem");
+    qmlRegisterType<DigitalItem>("Ardirec.Render", 1, 0, "DigitalItem");
 
     DocumentController document;
     QQmlApplicationEngine engine;
