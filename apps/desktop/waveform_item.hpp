186ebe21fbafc63d13fdd4b86889142f0d1393b4
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 20:06:21 +0700

    feat: render arbitrary synchronized analog tracks

--- apps/desktop/waveform_item.hpp
+++ apps/desktop/waveform_item.hpp
@@ -3,6 +3,7 @@
 
 #include "document_controller.hpp"
 
+#include <QColor>
 #include <QPointer>
 #include <QQuickItem>
 
@@ -11,6 +12,8 @@
 class WaveformItem : public QQuickItem {
     Q_OBJECT
     Q_PROPERTY(QObject* document READ document WRITE setDocument NOTIFY documentChanged)
+    Q_PROPERTY(int channelIndex READ channelIndex WRITE setChannelIndex NOTIFY channelIndexChanged)
+    Q_PROPERTY(QColor traceColor READ traceColor WRITE setTraceColor NOTIFY traceColorChanged)
     Q_PROPERTY(double zoomFactor READ zoomFactor WRITE setZoomFactor NOTIFY viewChanged)
     Q_PROPERTY(double panFraction READ panFraction WRITE setPanFraction NOTIFY viewChanged)
 public:
@@ -19,6 +22,11 @@ public:
     QObject* document() const { return m_document.data(); }
     void setDocument(QObject* document);
 
+    int channelIndex() const { return m_channelIndex; }
+    void setChannelIndex(int value);
+    QColor traceColor() const { return m_traceColor; }
+    void setTraceColor(const QColor& value);
+
     double zoomFactor() const { return m_zoomFactor; }
     void setZoomFactor(double value);
     double panFraction() const { return m_panFraction; }
@@ -26,6 +34,8 @@ public:
 
 signals:
     void documentChanged();
+    void channelIndexChanged();
+    void traceColorChanged();
     void viewChanged();
 
 protected:
@@ -35,7 +45,10 @@ private:
     void reloadSamples();
 
     QPointer<DocumentController> m_document;
+    int m_channelIndex{0};
+    QColor m_traceColor{QStringLiteral("#406a9b")};
     std::vector<double> m_samples;
+    std::vector<double> m_times;
     double m_zoomFactor{1.0};
     double m_panFraction{0.0};
 };
