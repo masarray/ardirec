d09eb43b3e9c862cbbed030cc28d965b2606e82a
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-23 12:19:45 +0700

    release: bump desktop version to alpha.8

--- apps/desktop/main.cpp
+++ apps/desktop/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     QGuiApplication app(argc, argv);
     QGuiApplication::setApplicationName(QStringLiteral("ardirec"));
     QGuiApplication::setOrganizationName(QStringLiteral("ardirec"));
-    QGuiApplication::setApplicationVersion(QStringLiteral("0.2.0-alpha.7"));
+    QGuiApplication::setApplicationVersion(QStringLiteral("0.2.0-alpha.8"));
 
     qmlRegisterType<WaveformItem>("Ardirec.Render", 1, 0, "WaveformItem");
     qmlRegisterType<RmsWaveformItem>("Ardirec.Render", 1, 0, "RmsWaveformItem");
