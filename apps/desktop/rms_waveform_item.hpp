2b2d3cfd8b592053b5e1e67df4a69006beb62761
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-13 08:08:06 +0700

    P0.1: prepare async RMS geometry state

--- apps/desktop/rms_waveform_item.hpp
+++ apps/desktop/rms_waveform_item.hpp
@@ -6,9 +6,13 @@
 #include <QColor>
 #include <QPointer>
 #include <QQuickItem>
+#include <QTimer>
 
+#include <atomic>
 #include <memory>
 #include <vector>
+
+struct RmsGeometrySnapshot;
 
 class RmsWaveformItem : public QQuickItem {
     Q_OBJECT
@@ -19,6 +23,7 @@ class RmsWaveformItem : public QQuickItem {
     Q_PROPERTY(double panFraction READ panFraction WRITE setPanFraction NOTIFY viewChanged)
 public:
     explicit RmsWaveformItem(QQuickItem* parent = nullptr);
+    ~RmsWaveformItem() override;
 
     QObject* document() const { return m_document.data(); }
     void setDocument(QObject* document);
@@ -41,14 +46,22 @@ signals:
 
 protected:
     QSGNode* updatePaintNode(QSGNode* oldNode, UpdatePaintNodeData*) override;
+    void geometryChange(const QRectF& newGeometry, const QRectF& oldGeometry) override;
 
 private:
     void reloadData();
     void refreshRepresentation();
+    void scheduleRebuild();
+    void startRebuild();
+    void clearPreparedGeometry();
 
     QPointer<DocumentController> m_document;
     std::shared_ptr<const ardirec::comtrade::IndexedDatFile> m_data;
     std::shared_ptr<const std::vector<double>> m_times;
+    std::shared_ptr<const RmsGeometrySnapshot> m_renderSnapshot;
+    std::shared_ptr<std::atomic_bool> m_activeCancel;
+    QTimer m_rebuildTimer;
+    quint64 m_rebuildGeneration{0};
     int m_channelIndex{0};
     QColor m_traceColor{QStringLiteral("#406a9b")};
     double m_displayScale{1.0};
