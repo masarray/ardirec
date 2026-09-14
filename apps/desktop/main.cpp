bf3ac7204f5ccd8ed5cabef661f0d390a2372ee8
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-14 07:38:46 +0700

    R4: bump qualification candidate to alpha.22

--- apps/desktop/main.cpp
+++ apps/desktop/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char* argv[]) {
                                             ? QStringLiteral("ARSAS COMTRADE Viewer")
                                             : QStringLiteral("ardirec"));
     QGuiApplication::setOrganizationName(QStringLiteral("ardirec"));
-    QGuiApplication::setApplicationVersion(QStringLiteral("0.2.0-alpha.21"));
+    QGuiApplication::setApplicationVersion(QStringLiteral("0.2.0-alpha.22"));
 
     qmlRegisterType<WaveformItem>("Ardirec.Render", 1, 0, "WaveformItem");
     qmlRegisterType<RmsWaveformItem>("Ardirec.Render", 1, 0, "RmsWaveformItem");
